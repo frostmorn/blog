0422b29c9d504687b1d370e7ac4c6151cf5458fc
Orphey <15167344+frostmorn@users.noreply.github.com>
2021-02-14 21:48:54 +0000

    Basic session handling, posts list, css improvements

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,8 +1,13 @@
 import os
+
 from . import db
 from flask import render_template
 from flask import Flask
 from flask import request
+from flask import session
+from flask import redirect
+from flask import url_for
+from flask import jsonify
 import bcrypt
 
 def create_app(test_config=None):
@@ -28,20 +33,22 @@ def create_app(test_config=None):
 
     @app.route('/login', methods=['GET', 'POST'])
     def login():
-    # TODO: Add password hashing    
         message = ""
         if request.method == 'POST':
             username = request.form['username']
             password = request.form['password']
             database = db.get_db()
-            user_hashed_password = database.execute(
-                'SELECT password FROM user WHERE username = ?', (username,)
-            ).fetchone()[0]
-            if user:
+            user_password_query = database.execute(
+                'SELECT * FROM user WHERE username = ?', (username,)
+            ).fetchone()
+            if user_password_query:
+                user_hashed_password = user_password_query['password']
                 print("User is trying to login. Username = {u}, Password = {p}\r\n".format(u=username, p=password))
 
                 if bcrypt.checkpw(bytes(password, "utf-8"), bytes(bytearray.fromhex(user_hashed_password))):
-                    message = "Login complete"
+			#todo make it okay, u got me?
+                    session['user-id'] = user_password_query['id']
+                    return redirect(url_for('index'))
                 else:
                     message = "Login error"
             else:
@@ -65,26 +72,45 @@ def create_app(test_config=None):
                 hashed_password = bcrypt.hashpw(bytes(password, "utf-8"), bcrypt.gensalt()).hex()
                 print("No user found")
                 print("Trying to reg User: \r\nusername = {u}, password = {p}, hashed={h}".format(u=username, p=password, h=hashed_password))
-                hz = database.execute('INSERT INTO user ("id","username","password") VALUES (NULL,?,?)', (username, hashed_password,)).fetchone()
+                user_query = database.execute('INSERT INTO user ("id","username","password") VALUES (NULL,?,?)', (username, hashed_password,)).fetchone()
                 database.commit()
                 # return "Registration complete!"
-                message = "Registration complete !"
-                return render_template('blog/auth/register.html', message=message )
+                # message = "Registration complete !"
+                # return render_template('blog/auth/register.html', message=message )
+                
+                # session['user-id'] = user_query['id']
+                return redirect(url_for('index'))
             else:
                 message = "User {u} allready exists !".format(u=username)
                 return render_template('blog/auth/register.html', message=message)
             
         else:
             return render_template('blog/auth/register.html')
-
+    @app.route('/logout')
+    def logout():
+        session.clear()
+        return redirect(url_for('index'))
+        
     
     @app.route('/')
     def index():
-        return render_template('base.html')
+        return redirect(url_for('list'))
 
     @app.route('/list')
     def list():
-        return render_template('blog/base.html')
+        database = db.get_db()
+        posts = user = database.execute('SELECT * FROM post')
+        return render_template('blog/list.html', posts=posts)
+    @app.route('/add', methods=['GET', 'POST'])
+    def add():
+        if request.method == 'POST':
+            if 'user-id' in session:
+                title = request.form['title']
+                content = request.form['content']
+                database = db.get_db()
+                hz = database.execute('INSERT INTO post ("id","title","content", "author_id") VALUES (NULL,?,?, ?)', (title, content,int(session['user-id']))).fetchone()
+                database.commit()
+        return render_template('blog/add.html', message = "Okay we done there")
 
     @app.route('/show/<int:page>')
     def show(page):
---
+++ init.sh
@@ -0,0 +1,1 @@
+FLASK_APP=blog flask init-db
